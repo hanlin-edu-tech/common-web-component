86e5873213c2a0e8bc0ac65d537b2a46a9432172
infinityAlive <sonic@ehanlin.com.tw>
2017-07-14 16:46:39 +0800

    修正上傳檔案的路徑

--- uploadS3.js
+++ uploadS3.js
@@ -4,7 +4,6 @@ const PATH = require("path");
 const EHANLIN_S3_ID = process.env.EHANLIN_S3_ID
 const EHANLIN_S3_KEY = process.env.EHANLIN_S3_KEY
 const TRAVIS_TAG = process.env.TRAVIS_TAG
-const TRAVIS_BUILD_DIR = process.env.TRAVIS_BUILD_DIR
 
 AWS.config.update({
   accessKeyId: EHANLIN_S3_ID,
@@ -13,43 +12,52 @@ AWS.config.update({
 
 const AWS_S3 = new AWS.S3();
 
-// 遞迴上傳檔案
-var recursiveUpload = dir => {
+// 尋找目標資料夾
+var findDist = dir => {
   FS.readdir(dir, (err, files) => {
-    // 不上傳的檔案
-    var isNotUpload = (FS, entireFilePath, fileName) => {
-      if (FS.lstatSync(entireFilePath).isDirectory()) {
-        recursiveUpload(entireFilePath);
-        return true;
-      }
-      
-      var notUploadFiles = ['README.md', '.travis.yml', 'uploadS3.js', 'package.json']
-      for (var notUploadFile of notUploadFiles) {
-        if (fileName === notUploadFile)
-          return true;
-      }
-
-      return false;
-    }
-
     var entireFilePath;
-
-    if (!files || files.length === 0) {
-      console.log(`${files} is not found or empty...`);
+    if(determineFileEmpty(files))
       return;
-    }
 
     files.forEach(fileName => {
       entireFilePath = PATH.join(dir, fileName);
-      console.log('before->' + entireFilePath);
-      if (isNotUpload(FS, entireFilePath, fileName)) {
+
+      if (fileName !== 'dist') {
+        if (FS.statSync(entireFilePath).isDirectory()) {
+          findDist(entireFilePath);
+          return;
+        }
+      }
+      else {
+        // dist 的前一層目錄
+        var saveDir = PATH.basename(dir);
+        upload(dir, saveDir);
+      }
+    });
+  });
+}
+
+
+// 上傳檔案
+var upload = (dir, saveDir) => {
+  FS.readdir(dir, (err, files) => {
+    if(determineFileEmpty(files))
+      return;
+
+    files.forEach(fileName => {
+      entireFilePath = PATH.join(dir, fileName);
+      if (FS.statSync(entireFilePath).isDirectory()) {
+        upload(entireFilePath, saveDir);
         return;
       }
 
+      // 把路徑到 dist 的位置，以前一層目錄取代
+      var savePATH = entireFilePath.replace(/[\w\/-]*(dist)/, saveDir);
+      
       AWS_S3.putObject({
         Bucket: 'ehanlin-web-resource',
         Body: FS.readFileSync(entireFilePath),
-        Key: `common_webcomponent/${TRAVIS_TAG}/${fileName}`,
+        Key: `common_webcomponent/${TRAVIS_TAG}/${savePATH}`,
         ACL: 'public-read'
       }).on('httpUploadProgress', function (progress) {
         // 上傳的進程
@@ -62,4 +70,13 @@ var recursiveUpload = dir => {
   });
 };
 
-recursiveUpload(TRAVIS_BUILD_DIR);
+var determineFileEmpty = files => {
+  if (!files || files.length === 0) {
+    console.log(`${files} is not found or empty...`);
+    return true;
+  }
+
+  return false;
+}
+
+findDist(__dirname);
