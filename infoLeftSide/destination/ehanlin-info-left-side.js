0514ff5d2ed99b201a9facb5065b74f76b9992d4
AsoTiesto <cute17852337569@gmail.com>
2017-07-27 15:17:08 +0800

    add info left side html and js

---
+++ infoLeftSide/destination/ehanlin-info-left-side.js
@@ -0,0 +1,22 @@
+(function() {
+  fetch(
+    "https://s3-ap-northeast-1.amazonaws.com/ehanlin-web-resource/common_webcomponent/v0.0.1/infoLeftSide/ehanlin_info_left_side.html",
+    {
+      method: "get",
+      mode: "cors"
+    }
+  )
+    .then(function(response) {
+      if (!response.ok) {
+        throw new Error("Network response was not ok.");
+      }
+      response.text().then(function(text) {
+        var banner = document.createRange().createContextualFragment(text);
+        var parent = document.getElementById("info-left-side");
+        parent.insertBefore(banner, parent.firstChild);
+      });
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+})();
