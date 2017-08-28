6ae469e8118398ca88eadd3ec88764a19aaef9dc
infinityAlive <sonic@ehanlin.com.tw>
2017-08-28 12:06:36 +0800

    暫時新增 console.log

--- eventLeftSide/destination/ehanlin-event-left-side.js
+++ eventLeftSide/destination/ehanlin-event-left-side.js
@@ -1,5 +1,7 @@
 (function() {
   var link = decodeURIComponent(location.pathname);
+  console.log(location.pathname);
+  console.log(link);
   var targetAnchor = document.querySelector(
     ".menu li > a[href*='" + link + "']"
   );
