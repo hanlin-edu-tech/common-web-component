f76a6c4f3a2b50ff8d04737b3a71774c6e17bbd4
AsoTiesto <cute17852337569@gmail.com>
2017-08-30 18:49:37 +0800

    新增json 輔助changeCurrentPath.js 讀取檔案

--- deploy/changeCurrentPath.js
+++ deploy/changeCurrentPath.js
@@ -5,52 +5,44 @@ const EHANLIN_S3_ID = process.env.EHANLIN_S3_ID;
 const EHANLIN_S3_KEY = process.env.EHANLIN_S3_KEY;
 const TRAVIS_TAG = process.env.TRAVIS_TAG;
 const AWS_S3 = new AWS.S3();
-var sourceDir = PATH.join(__dirname, "../");
+var obj = JSON.parse(FS.readFileSync("../addFileUsingJson.json", "UTF-8"));
+console.log(obj);
 
 AWS.config.update({
   accessKeyId: EHANLIN_S3_ID,
   secretAccessKey: EHANLIN_S3_KEY
 });
 
+var readFileChangeContent = (filePath, fileName) => {
+  FS.readFile(filePath, "UTF-8", function(err, data) {
+    if (!err) {
+      console.log(filePath);
+      if (data.includes("current.SNAPSHOT")) {
+        var changeContent = data.replace(/current\.SNAPSHOT/g, "current");
+      } else if (data.includes("current")) {
+        var changeContent = data.replace(/current/g, "current.SNAPSHOT");
+      }
+      writeFileToFolder(filePath, changeContent, fileName);
+    } else {
+      console.log(err);
+    }
+  });
+};
+
+var writeFileToFolder = (filePath, changeContent, fileName) => {
+  FS.writeFile(filePath, changeContent, "UTF-8", err => {
+    if (err) throw err;
+    console.log(`The ${fileName} was succesfully saved!`);
+  });
+};
+
 /**
  * 更改元件內容路徑
  */
-var m = new Map();
-m.set("header", "ehanlin_header.html");
-m.set("footer", "ehanlin_footer.html");
-m.set("eventLeftSide", "ehanlin_event_left_side.html");
-m.set("infoLeftSide", "ehanlin_info_left_side.html");
-m.set("menu", "ehanlin_menu.html");
-m.set("js", "ehanlin-loader.js");
-
-m.forEach(function(item, key) {
+for (var key in obj) {
   var folderName = key.toString();
-  var fileName = item.toString();
+  var fileName = obj[key];
   var changeFilePath = `${folderName}/destination/${fileName}`;
-  var filePath = PATH.join(sourceDir, `${changeFilePath}`);
-
-  var readFileChangeContent = () => {
-    FS.readFile(filePath, "UTF-8", function(err, data) {
-      if (!err)
-        if (data.includes("current.SNAPSHOT")) {
-          var changeContent = data.replace(/current\.SNAPSHOT/g, "current");
-        } else if (data.includes("current")) {
-          var changeContent = data.replace(/current/g, "current.SNAPSHOT");
-        } else console.log(err);
-      writeFileToFolder(folderName, fileName, changeContent);
-    });
-  };
-  readFileChangeContent();
-});
-
-var writeFileToFolder = (folderName, fileName, changeContent) => {
-  FS.writeFile(
-    `../${folderName}/destination/${fileName}`,
-    changeContent,
-    "UTF-8",
-    err => {
-      if (err) throw err;
-      console.log("The file was succesfully saved!");
-    }
-  );
-};
+  var filePath = PATH.join(__dirname, `../${changeFilePath}`);
+  readFileChangeContent(filePath, fileName);
+}
