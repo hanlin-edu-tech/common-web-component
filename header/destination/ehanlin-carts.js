05726c6e2886caf77a36316e222e9b25baa721fb
AsoTiesto <cute17852337569@gmail.com>
2017-10-25 10:14:24 +0800

    test

--- header/destination/ehanlin-carts.js
+++ header/destination/ehanlin-carts.js
@@ -1,9 +1,14 @@
-require(["jquery"], function ($) {
-  $.get("/my/owned/Carts.json", {
-    ts: new Date().getTime()
-  }, function (resp) {
-    if (resp.success && resp.result) {
-      $("#car_sum").text(resp.result.items.length);
+require(["jquery"], function($) {
+  $.get(
+    "/my/owned/Carts.json",
+    {
+      ts: new Date().getTime()
+    },
+    function(resp) {
+      if (resp.success && resp.result) {
+        console.log("test");
+        $("#car_sum").text(resp.result.items.length);
+      }
     }
-  });
+  );
 });
