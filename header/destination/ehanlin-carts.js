09fd52139627c879321018ebf344ec22c645743a
infinityAlive <sonic@ehanlin.com.tw>
2017-10-25 10:38:26 +0800

    修正 jQuery 的相依版本

--- header/destination/ehanlin-carts.js
+++ header/destination/ehanlin-carts.js
@@ -1,12 +1,12 @@
-require(["jquery"], function($) {
-  $.get(
+require(["jquery3_2_1"], function(jquery3_2_1) {
+  jquery3_2_1.get(
     "/my/owned/Carts.json",
     {
       ts: new Date().getTime()
     },
     function(resp) {
       if (resp.success && resp.result) {
-        $("#car_sum").text(resp.result.items.length);
+        jquery3_2_1("#car_sum").text(resp.result.items.length);
       }
     }
   );
--- header/destination/ehanlin-marquee.js
+++ header/destination/ehanlin-marquee.js
@@ -1,21 +1,23 @@
 /**
  * ehanlin 跑馬燈
  */
-require(["jquery", "marquee"], function($, marquee) {
-  var $mq = $(".marquee");
-  $.get(
-    "https://test.ehanlin.com.tw/Marquee",
-    function(data) {
-      $.each(data, function(index, element) {
-        var liText = $("<li></li>")
-          .addClass("marquee-content")
-          .css({ "background-color": element.backgroundColor })
-          .html(element.text);
-        $(".marquee ul").append(liText);
-      });
-    },
-    "json"
-  ).done(function() {
-    $mq.marquee({ duration: 15000, direction: "left", duplicated: true });
-  });
+require(["jquery3_2_1", "marquee"], function(jquery3_2_1, marquee) {
+  var $mq = jquery3_2_1(".marquee");
+  jquery3_2_1
+    .get(
+      "https://test.ehanlin.com.tw/Marquee",
+      function(data) {
+        jquery3_2_1.each(data, function(index, element) {
+          var liText = jquery3_2_1("<li></li>")
+            .addClass("marquee-content")
+            .css({ "background-color": element.backgroundColor })
+            .html(element.text);
+          jquery3_2_1(".marquee ul").append(liText);
+        });
+      },
+      "json"
+    )
+    .done(function() {
+      $mq.marquee({ duration: 15000, direction: "left", duplicated: true });
+    });
 });
