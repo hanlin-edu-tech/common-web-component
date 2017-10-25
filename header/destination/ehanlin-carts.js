6dfd6fab008ed9daf230fd7aca083f1da8dd67e5
AsoTiesto <cute17852337569@gmail.com>
2017-10-25 09:58:38 +0800

    移除console 訊息 #3

--- header/destination/ehanlin-carts.js
+++ header/destination/ehanlin-carts.js
@@ -1,7 +1,8 @@
-require(["jquery"], function($) {
-  $.get("/my/owned/Carts.json", { ts: new Date().getTime() }, function(resp) {
+require(["jquery"], function ($) {
+  $.get("/my/owned/Carts.json", {
+    ts: new Date().getTime()
+  }, function (resp) {
     if (resp.success && resp.result) {
-      console.log(resp.result.items.length);
       $("#car_sum").text(resp.result.items.length);
     }
   });
