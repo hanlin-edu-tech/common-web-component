baad59e1ba17b597eb8a40735504c39adf329b4f
infinityAlive <sonic@ehanlin.com.tw>
2017-10-26 19:07:47 +0800

    fix 登出路徑

--- header/destination/ehanlin-userstatus.js
+++ header/destination/ehanlin-userstatus.js
@@ -23,7 +23,7 @@ define(["jQuery3_noConflict"], function(jQuery3_noConflict) {
       logoutPut("/Users/521d946be4b0d765448570bd/!logout", null, function(
         data
       ) {
-        location.href = location.hostname;
+        location = "https://" + location.hostname;
       });
       return false;
     });
