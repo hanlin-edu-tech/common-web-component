844dbcbb421e229eb1b113be91a247dff77ce182
infinityAlive <sonic@ehanlin.com.tw>
2017-12-22 16:06:14 +0800

    跑馬燈 css 樣式微調，增加 close 按鈕

--- header/destination/ehanlin-marquee.js
+++ header/destination/ehanlin-marquee.js
@@ -1,28 +1,40 @@
 /**
  * ehanlin 跑馬燈
  */
-define(["jQueryNoConflict"], function(
-  jQueryNoConflict
-) {
+define(["jQueryNoConflict"], function (jQueryNoConflict) {
   var marqueeTarget = jQueryNoConflict("#ehanlin-header div.marquee");
-  jQueryNoConflict.get("/Marquee", function(data) {
-        if (data && data.length > 0) {
-          marqueeTarget.removeAttr("style");
-          jQueryNoConflict.each(data, function(index, element) {
-            var liText = jQueryNoConflict("<li></li>")
-              .addClass("marquee-content")
-              .css({ "background-color": element.backgroundColor })
-              .html(element.text);
+  var marqueeCloseTarget = jQueryNoConflict("#ehanlin-header i.marquee-close");
+  var isShowMarqueeClose = true;
 
-            marqueeTarget.css({ "background-color": element.backgroundColor });
-            marqueeTarget.find("ul").append(liText);
-          });
-        }
-      },
-      "json"
-    )
-    .done(function() {
-      require("marquee");
-      marqueeTarget.marquee({duration: 11000});
-    });
+  jQueryNoConflict.get("/Marquee", function (data) {
+      if ( data && data.length > 0 ) {
+        marqueeTarget.removeAttr("style");
+
+        jQueryNoConflict.each(data, function (index, singleMarquee) {
+          if ( singleMarquee.type === "system" ) {
+            isShowMarqueeClose = false;
+          }
+
+          var liText = jQueryNoConflict("<li></li>")
+            .addClass("marquee-content")
+            .css({ "background-color": singleMarquee.backgroundColor })
+            .html(singleMarquee.text);
+
+          marqueeTarget.css({ "background-color": singleMarquee.backgroundColor });
+          marqueeTarget.find("ul").append(liText);
+        });
+      }
+    },
+    "json"
+  ).done(function () {
+    require("marquee");
+    marqueeTarget.marquee({ duration: 15000 });
+    if ( isShowMarqueeClose === true ) {
+      marqueeCloseTarget.removeAttr("style");
+    }
+  });
+
+  jQueryNoConflict("#ehanlin-header i.marquee-close").on("click", function () {
+    jQueryNoConflict(this).parents("#ehanlin-marquee").remove();
+  });
 });
