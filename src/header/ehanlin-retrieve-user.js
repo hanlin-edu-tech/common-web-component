e6581adfe44ebb929746acd7ae46c868e016f704
AsoTiesto <cute17852337569@gmail.com>
2018-01-31 11:17:27 +0800

    test

--- src/header/ehanlin-retrieve-user.js
+++ src/header/ehanlin-retrieve-user.js
@@ -1,9 +1,9 @@
-import { jQueryNoConflict } from './jquery-no-conflict'
+import { jQueryNoConflict } from "./jquery-no-conflict";
 
 let ajaxUtil = (type, url, data) => {
-  if (type !== 'GET') {
+  if (type !== "GET") {
     if (data) {
-      data = JSON.stringify(data)
+      data = JSON.stringify(data);
     }
   }
 
@@ -13,46 +13,45 @@ let ajaxUtil = (type, url, data) => {
     crossDomain: true,
     url: url,
     data: data,
-    contentType: 'application/json; charset=UTF-8',
-    dataType: 'json'
-  })
-}
+    contentType: "application/json; charset=UTF-8",
+    dataType: "json"
+  });
+};
 
 let logout = () => {
-  jQueryNoConflict('#logoutButton').on('click', () => {
-    ajaxUtil('PUT', '/Users/521d946be4b0d765448570bd/!logout')
-      .then(() => {
-        window.location = 'https://' + window.location.hostname
-      })
-  })
-}
+  jQueryNoConflict("#logoutButton").on("click", () => {
+    ajaxUtil("PUT", "/Users/521d946be4b0d765448570bd/!logout").then(() => {
+      window.location = "https://" + window.location.hostname;
+    });
+  });
+};
 
 let logIn = () => {
   let success = data => {
-    let userId = data['id']
-    let studentCard = data['studentCard']
-    let name = data.name
+    let userId = data["id"];
+    let studentCard = data["studentCard"];
+    let name = data.name;
     let userInfoHtml = `<a href='/Users/${userId}.html'> ${studentCard} ${name} </a>
         <span style='color:#9B9B9B'> | </span>
         <li><a id='logoutButton'>登出</a></li>
-        <span style='color:#9B9B9B'> | </span>`
+        <span style='color:#9B9B9B'> | </span>`;
 
-    jQueryNoConflict('#loginSuccess').append(userInfoHtml)
-    jQueryNoConflict('#loginBotton').remove()
-    jQueryNoConflict('#register').remove()
-    logout()
-  }
+    jQueryNoConflict("#loginSuccess").append(userInfoHtml);
+    jQueryNoConflict("#loginBotton").remove();
+    jQueryNoConflict("#register").remove();
+    logout();
+  };
 
   let error = () => {
-    jQueryNoConflict('ul.header-menu').removeAttr('style')
-  }
+    jQueryNoConflict("ul.header-menu").removeAttr("style");
+  };
 
-  ajaxUtil('GET', '/ms-user-status/userStatus')
+  ajaxUtil("GET", "/ms-user-status/userStatus")
     .then(success, error)
     .then(() => {
-      jQueryNoConflict('ul.header-menu').removeAttr('style')
-    })
-}
+      jQueryNoConflict("ul.header-menu").removeAttr("style");
+    });
+};
 
-export { jQueryNoConflict }
-export { logIn }
+export { jQueryNoConflict };
+export { logIn };
