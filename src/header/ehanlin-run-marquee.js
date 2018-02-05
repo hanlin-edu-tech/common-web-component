1432622c7c8aa853945799e41ab1959465a27c57
infinityAlive <sonic@ehanlin.com.tw>
2018-02-05 14:54:51 +0800

    修正 marguee 無法顯示

--- src/header/ehanlin-run-marquee.js
+++ src/header/ehanlin-run-marquee.js
@@ -14,8 +14,6 @@ let runMarquee = () => {
     if (data && data.length > 0) {
       let liText
       marqueeTarget.removeAttr('style')
-      marqueeCloseTarget.removeAttr('style')
-
       jQueryNoConflict.each(data, (index, singleMarquee) => {
         if (singleMarquee.type === 'system') {
           isShowMarqueeClose = false
