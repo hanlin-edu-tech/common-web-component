2b0b0ad7ea5c00033ffc147af2d8655d5f474eb4
AsoTiesto <cute17852337569@gmail.com>
2018-02-07 12:03:54 +0800

    test

--- src/header/destination/ehanlin-marquee.js
+++ src/header/destination/ehanlin-marquee.js
@@ -1,40 +1,48 @@
 /**
  * ehanlin 跑馬燈
  */
-define(["jQueryNoConflict"], function (jQueryNoConflict) {
-  var marqueeTarget = jQueryNoConflict("#ehanlin-header div.marquee");
-  var marqueeCloseTarget = jQueryNoConflict("#ehanlin-header i.marquee-close");
-  var isShowMarqueeClose = true;
+define(['jQueryNoConflict'], function (jQueryNoConflict) {
+  var marqueeTarget = jQueryNoConflict('#ehanlin-header div.marquee')
+  var marqueeCloseTarget = jQueryNoConflict('#ehanlin-header i.marquee-close')
+  var isShowMarqueeClose = true
 
-  jQueryNoConflict.get("/Marquee", function (data) {
-      if ( data && data.length > 0 ) {
-        marqueeTarget.removeAttr("style");
+  jQueryNoConflict
+    .get(
+      '/Marquee',
+      function (data) {
+        if (data && data.length > 0) {
+          marqueeTarget.removeAttr('style')
 
-        jQueryNoConflict.each(data, function (index, singleMarquee) {
-          if ( singleMarquee.type === "system" ) {
-            isShowMarqueeClose = false;
-          }
+          jQueryNoConflict.each(data, function (index, singleMarquee) {
+            if (singleMarquee.type === 'system') {
+              isShowMarqueeClose = false
+            }
 
-          var liText = jQueryNoConflict("<li></li>")
-            .addClass("marquee-content")
-            .css({ "background-color": singleMarquee.backgroundColor })
-            .html(singleMarquee.text);
+            var liText = jQueryNoConflict('<li></li>')
+              .addClass('marquee-content')
+              .css({ 'background-color': singleMarquee.backgroundColor })
+              .html(singleMarquee.text)
 
-          marqueeTarget.css({ "background-color": singleMarquee.backgroundColor });
-          marqueeTarget.find("ul").append(liText);
-        });
+            marqueeTarget.css({
+              'background-color': singleMarquee.backgroundColor
+            })
+            marqueeTarget.find('ul').append(liText)
+          })
+        }
+      },
+      'json'
+    )
+    .done(function () {
+      require('marquee')
+      marqueeTarget.marquee({ duration: 15000 })
+      if (isShowMarqueeClose === true) {
+        marqueeCloseTarget.removeAttr('style')
       }
-    },
-    "json"
-  ).done(function () {
-    require("marquee");
-    marqueeTarget.marquee({ duration: 15000 });
-    if ( isShowMarqueeClose === true ) {
-      marqueeCloseTarget.removeAttr("style");
-    }
-  });
+    })
 
-  jQueryNoConflict("#ehanlin-header i.marquee-close").on("click", function () {
-    jQueryNoConflict(this).parents("#ehanlin-marquee").remove();
-  });
-});
+  jQueryNoConflict('#ehanlin-header i.marquee-close').on('click', function () {
+    jQueryNoConflict(this)
+      .parents('#ehanlin-marquee')
+      .remove()
+  })
+})
--- src/header/ehanlin-run-marquee.js
+++ src/header/ehanlin-run-marquee.js
@@ -3,43 +3,52 @@ import 'jquery.marquee'
 
 let marqueeTarget = jQueryNoConflict('#ehanlin-header div.marquee')
 let marqueeCloseTarget = jQueryNoConflict('#ehanlin-header i.marquee-close')
-let isShowMarqueeClose = true
+let isShowMarqueeClose
 
 let runMarquee = () => {
-  jQueryNoConflict.ajax({
-    type: 'get',
-    url: '/Marquee',
-    dataType: 'json'
-  }).then(data => {
-    if (data && data.length > 0) {
-      let liText
-      marqueeTarget.removeAttr('style')
-      jQueryNoConflict.each(data, (index, singleMarquee) => {
-        if (singleMarquee.type === 'system') {
-          isShowMarqueeClose = false
-        }
-
-        liText = jQueryNoConflict('<li style="display: none"></li>')
-          .addClass('marquee-content')
-          .css({'background-color': singleMarquee.backgroundColor})
-          .html(singleMarquee.text)
-
-        marqueeTarget.css({'background-color': singleMarquee.backgroundColor})
-        marqueeTarget.find('ul').append(liText)
-      })
-    }
-  }).then(() => {
-    jQueryNoConflict('#ehanlin-header li.marquee-content').show()
-
-    marqueeTarget.marquee({duration: 15000})
-    if (isShowMarqueeClose === true) {
-      marqueeCloseTarget.removeAttr('style')
-    }
-  }).then(() => {
-    jQueryNoConflict('#ehanlin-header i.marquee-close').on('click', event => {
-      jQueryNoConflict(event.currentTarget).parents('#ehanlin-marquee').remove()
+  jQueryNoConflict
+    .ajax({
+      type: 'get',
+      url: '/Marquee',
+      dataType: 'json'
     })
-  })
+    .then(data => {
+      isShowMarqueeClose = true
+      if (data && data.length > 0) {
+        let liText
+        marqueeTarget.removeAttr('style')
+        jQueryNoConflict.each(data, (index, singleMarquee) => {
+          if (singleMarquee.type === 'system') {
+            isShowMarqueeClose = false
+          }
+
+          liText = jQueryNoConflict('<li style="display: none"></li>')
+            .addClass('marquee-content')
+            .css({ 'background-color': singleMarquee.backgroundColor })
+            .html(singleMarquee.text)
+
+          marqueeTarget.css({
+            'background-color': singleMarquee.backgroundColor
+          })
+          marqueeTarget.find('ul').append(liText)
+        })
+      }
+    })
+    .then(() => {
+      jQueryNoConflict('#ehanlin-header li.marquee-content').show()
+
+      marqueeTarget.marquee({ duration: 15000 })
+      if (isShowMarqueeClose === true) {
+        marqueeCloseTarget.removeAttr('style')
+      }
+    })
+    .then(() => {
+      jQueryNoConflict('#ehanlin-header i.marquee-close').on('click', event => {
+        jQueryNoConflict(event.currentTarget)
+          .parents('#ehanlin-marquee')
+          .remove()
+      })
+    })
 }
 
 export { runMarquee }
