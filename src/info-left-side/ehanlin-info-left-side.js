bcd311da432c0b054c04f289c2e6bbaca075fb92
AsoTiesto <cute17852337569@gmail.com>
2018-10-22 14:42:29 +0800

    修復bug：修復 gsatanalysis 活動頁無法收合問題

--- src/info-left-side/ehanlin-info-left-side.js
+++ src/info-left-side/ehanlin-info-left-side.js
@@ -15,7 +15,7 @@
     pathname.match('12y_edu.html') || pathname.match('hsapp.html')) {
     document.querySelector('.senior').style.display = 'none'
     document.querySelector('.university').style.display = 'none'
-  } else if (pathname.match('my_test_edu.html')) {
+  } else if (pathname.match('my_test_edu.html') || pathname.match('gsatanalysis')) {
     document.querySelector('.junior').style.display = 'none'
     document.querySelector('.university').style.display = 'none'
   } else if (pathname.match('specify_exam_edu.html')) {
