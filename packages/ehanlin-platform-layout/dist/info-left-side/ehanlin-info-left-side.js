cadbbad4627dc12f844c4b3a7040c2b292d8712c
infinityAlive <sonicx7boyboyboy@gmail.com>
2019-05-08 15:33:14 +0800

    更新相關元件與 Fixed info-left-menu 尋找不到當前功能

--- packages/ehanlin-platform-layout/dist/info-left-side/ehanlin-info-left-side.js
+++ packages/ehanlin-platform-layout/dist/info-left-side/ehanlin-info-left-side.js
@@ -1,6 +1,8 @@
 (function () {
   var e = decodeURIComponent(window.location.search),
       o = decodeURIComponent(window.location.hash),
-      n = decodeURIComponent(window.location.pathname),
-      c = document.querySelector(".menu ul li > a[href*='" + n + o + "']") || document.querySelector(".menu ul li > a[href*='" + n + o + e + "']");c && c.classList.add("select");
+      c = decodeURIComponent(window.location.pathname),
+      n = document.querySelector(".menu ul li > a[href*='" + c + o + "']") || document.querySelector(".menu ul li > a[href*='" + c + o + e + "']");if (n) {
+    var _e = document.querySelector(".menu ul li > a.select");_e && _e.classList.remove("select"), n.classList.contains("select") || n.classList.add("select");
+  }
 })();
--- packages/ehanlin-platform-layout/src/info-left-side/ehanlin-info-left-side.js
+++ packages/ehanlin-platform-layout/src/info-left-side/ehanlin-info-left-side.js
@@ -3,23 +3,18 @@
   const hash = decodeURIComponent(window.location.hash)
   const pathname = decodeURIComponent(window.location.pathname)
 
-  const targetAnchor =
+  const currentAnchor =
     document.querySelector(`.menu ul li > a[href*='${pathname}${hash}']`)
     || document.querySelector(`.menu ul li > a[href*='${pathname}${hash}${search}']`)
 
-  if (targetAnchor) {
-    targetAnchor.classList.add('select')
-  }
+  if (currentAnchor) {
+    const selectedAnchor = document.querySelector('.menu ul li > a.select')
+    if (selectedAnchor) {
+      selectedAnchor.classList.remove('select')
+    }
 
-  // if (pathname.match('108cap') || pathname.match('105jrhighexam.html') ||
-  //   pathname.match('12y_edu.html') || pathname.match('hsapp.html')) {
-  //   document.querySelector('.senior').style.display = 'none'
-  //   document.querySelector('.university').style.display = 'none'
-  // } else if (pathname.match('my_test_edu.html') || pathname.match('gsatanalysis')) {
-  //   document.querySelector('.junior').style.display = 'none'
-  //   document.querySelector('.university').style.display = 'none'
-  // } else if (pathname.match('specify_exam_edu.html')) {
-  //   document.querySelector('.junior').style.display = 'none'
-  //   document.querySelector('.senior').style.display = 'none'
-  // }
+    if (!currentAnchor.classList.contains('select')) {
+      currentAnchor.classList.add('select')
+    }
+  }
 })()
