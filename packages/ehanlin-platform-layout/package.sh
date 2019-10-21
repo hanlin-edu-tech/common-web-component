7c8d73e16b99a2f46e167bedf4d59f993ad351a8
AsoTiesto <cute17852337569@gmail.com>
2019-10-21 14:52:06 +0800

    調整功能：更換 gcs連結

--- packages/ehanlin-platform-layout/package.sh
+++ packages/ehanlin-platform-layout/package.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 gulp packageToTest
-gulp replaceEnvTest
 gulp uploadGcsTest
--- packages/ehanlin-platform-layout/src/js/ehanlin-loader.js
+++ packages/ehanlin-platform-layout/src/js/ehanlin-loader.js
@@ -1,120 +1,89 @@
-(function () {
-  var ajaxUtils = {
-    retrieveHtml: function (componentPath, httpMethod, id) {
-      var url = 'https://s3-ap-northeast-1.amazonaws.com/ehanlin-web-resource/common_webcomponent/' +
-        componentPath
-      var request = new XMLHttpRequest()
+(function() {
+    var ajaxUtils = {
+        retrieveHtml: function(componentPath, httpMethod, id, envComponentDir) {
+            var url = "https://storage.cloud.google.com/" + envComponentDir + "/app/web-component" + componentPath + "?authuser=1&folder=true&organizationId=true";
+            var request = new XMLHttpRequest();
 
-      if (!id) return
+            console.log(url);
 
-      request.open(httpMethod, url, true)
-      request.send()
-      request.onload = function () {
-        var parent, scripts, script, newScript
-        var timestamp = new Date().getTime()
+            if (!id) return;
 
-        if (request.status >= 200 && request.status < 400) {
-          parent = document.getElementById(id)
-          if (!parent) return
+            request.open(httpMethod, url, true);
+            request.send();
+            request.onload = function() {
+                var parent, scripts, script, newScript;
+                var timestamp = new Date().getTime();
 
-          parent.insertAdjacentHTML('beforeend', request.responseText)
+                if (request.status >= 200 && request.status < 400) {
+                    parent = document.getElementById(id);
+                    if (!parent) return;
 
-          // 重新加載 component 頁的 js
-          scripts = parent.querySelectorAll('script')
-          if (scripts) {
-            for (var i = 0; i < scripts.length; i++) {
-              script = scripts[i]
-              newScript = document.createElement('script')
-              newScript.src = script.src + '?v=' + timestamp
-              if (script.getAttribute('data-main')) {
-                newScript.setAttribute(
-                  'data-main',
-                  script.getAttribute('data-main')
-                )
-              }
-              parent.removeChild(script)
-              parent.insertAdjacentElement('afterend', newScript)
+                    parent.insertAdjacentHTML("beforeend", request.responseText);
+
+                    // 重新加載 component 頁的 js
+                    scripts = parent.querySelectorAll("script");
+                    if (scripts) {
+                        for (var i = 0; i < scripts.length; i++) {
+                            script = scripts[i];
+                            newScript = document.createElement("script");
+                            newScript.src = script.src + "?v=" + timestamp;
+                            if (script.getAttribute("data-main")) {
+                                newScript.setAttribute("data-main", script.getAttribute("data-main"));
+                            }
+                            parent.removeChild(script);
+                            parent.insertAdjacentElement("afterend", newScript);
+                        }
+                    }
+                } else {
+                    console.log("error status = " + request.status);
+                }
+            };
+
+            if (request.readyState === request.DONE) {
+                console.log("一切 ok, 繼續解析");
+            } else {
+                console.log(request.status);
+                console.log("還沒完成");
             }
-          }
-        } else {
-          console.log('error status = ' + request.status)
+
+            request.onerror = function() {
+                console.error("Response error, readyState = " + this.req.readyState + ", status = " + this.req.status);
+            };
         }
-      }
+    };
 
-      request.onerror = function () {
-        console.error(
-          'Response error, readyState = ' +
-          this.req.readyState +
-          ', status = ' +
-          this.req.status
-        )
-      }
+    var envComponentDir = "tutor-apps",
+        ehanlinComponents;
+
+    if (window.location.href.indexOf("www.tbbt.com.tw") >= 0) {
+        console.log("!!! 目前在測試環境 !!!");
+        envComponentDir = "tutor-test-apps";
     }
-  }
 
-  var componentDir = 'current',
-    ehanlinComponents
-
-  if (window.location.href.indexOf('www.tbbt.com.tw') >= 0) {
-    console.log('!!! 目前在測試環境 !!!')
-    componentDir = 'current.SNAPSHOT'
-  }
-
-  if (window.location.href.indexOf('labs.ehanlin.com.tw') >= 0) {
-    console.log('!!! 目前在重構環境 !!!')
-    componentDir = 'current.SNAPSHOT'
-  }
-
-
-  ehanlinComponents = {
-    'ehanlin-menu': ajaxUtils.retrieveHtml.bind(
-      this,
-      componentDir + '/menu/ehanlin-menu.html',
-      'get',
-      'ehanlin-menu'
-    ),
-    'ehanlin-header': ajaxUtils.retrieveHtml.bind(
-      this,
-      componentDir + '/header/ehanlin-header.html',
-      'get',
-      'ehanlin-header'
-    ),
-    'ehanlin-footer': ajaxUtils.retrieveHtml.bind(
-      this,
-      componentDir + '/footer/ehanlin-footer.html',
-      'get',
-      'ehanlin-footer'
-    ),
-    'ehanlin-event-left-side': ajaxUtils.retrieveHtml.bind(
-      this,
-      componentDir + '/event-left-side/ehanlin-event-left-side.html',
-      'get',
-      'event-left-side'
-    ),
-    'ehanlin-left-side': ajaxUtils.retrieveHtml.bind(
-      this,
-      componentDir + '/event-left-side/ehanlin-event-left-side.html',
-      'get',
-      'event-left-side'
-    ),
-    'ehanlin-info-left-side': ajaxUtils.retrieveHtml.bind(
-      this,
-      componentDir + '/info-left-side/ehanlin-info-left-side.html',
-      'get',
-      'info-left-side'
-    )
-  }
-
-  var script = document.querySelector('script[data-module]')
-  var dataModules = script.getAttribute('data-module')
-  var execFunction
-
-  if (dataModules) {
-    dataModules = dataModules.split(',')
-    for (var i = 0; i < dataModules.length; i++) {
-      var dataModule = dataModules[i]
-      execFunction = ehanlinComponents[dataModule.trim()]
-      if (typeof execFunction === 'function') execFunction()
+    if (window.location.href.indexOf("labs.ehanlin.com.tw") >= 0) {
+        console.log("!!! 目前在重構環境 !!!");
+        envComponentDir = "tutor-test-apps";
     }
-  }
-})()
+
+    ehanlinComponents = {
+        "ehanlin-menu": ajaxUtils.retrieveHtml.bind(this, "/menu/ehanlin-menu.html", "GET", "ehanlin-menu", envComponentDir),
+        "ehanlin-header": ajaxUtils.retrieveHtml.bind(this, "/header/ehanlin-header.html", "GET", "ehanlin-header", envComponentDir),
+        "ehanlin-footer": ajaxUtils.retrieveHtml.bind(this, "/footer/ehanlin-footer.html", "GET", "ehanlin-footer", envComponentDir),
+        "ehanlin-event-left-side": ajaxUtils.retrieveHtml.bind(this, "/event-left-side/ehanlin-event-left-side.html", "GET", "event-left-side", envComponentDir),
+        "ehanlin-left-side": ajaxUtils.retrieveHtml.bind(this, "/event-left-side/ehanlin-event-left-side.html", "GET", "event-left-side", envComponentDir),
+        "ehanlin-info-left-side": ajaxUtils.retrieveHtml.bind(this, "/info-left-side/ehanlin-info-left-side.html", "GET", "info-left-side", envComponentDir)
+    };
+
+    var script = document.querySelector("script[data-module]");
+    var dataModules = script.getAttribute("data-module");
+    var execFunction;
+
+    if (dataModules) {
+        dataModules = dataModules.split(",");
+        for (var i = 0; i < dataModules.length; i++) {
+            var dataModule = dataModules[i];
+            execFunction = ehanlinComponents[dataModule.trim()];
+            if (typeof execFunction === "function") execFunction();
+        }
+    }
+})();
